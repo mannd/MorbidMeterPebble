0a392f1882a38b7529d53ecea590a206b1501d98
David Mann <mannd@epstudiossoftware.com>
2015-10-15 21:18:24 -0600

    Expanded header, added seconds.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <pebble.h>
 
 #define MM_TITLE "MorbidMeter"
+#define LOCAL_TIME_TS "Local Time"
+#define TS "TimeScale: "
+#define DEFAULT_HEADER MM_TITLE "\n" TS LOCAL_TIME_TS
     
 static Window *s_main_window;
 static TextLayer *s_time_layer;
@@ -16,15 +19,15 @@ static void update_time() {
   struct tm *tick_time = localtime(&temp);
 
   // Create a long-lived buffer
-  static char buffer[] = MM_TITLE "\n00:00";
+  static char buffer[] = DEFAULT_HEADER "\n00:00:00";
 
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
     //Use 2h hour format
-    strftime(buffer, sizeof(MM_TITLE "\n00:00"), MM_TITLE "\n%H:%M", tick_time);
+    strftime(buffer, sizeof(DEFAULT_HEADER "\n00:00:00"), DEFAULT_HEADER "\n%H:%M:%S", tick_time);
   } else {
     //Use 12 hour format
-    strftime(buffer, sizeof(MM_TITLE "\n00:00"), MM_TITLE "\n%I:%M", tick_time);
+    strftime(buffer, sizeof(DEFAULT_HEADER "\n00:00:00"), DEFAULT_HEADER "\n%I:%M:%S", tick_time);
   }
 
   // Display this time on the TextLayer
@@ -33,22 +36,23 @@ static void update_time() {
 
 static void main_window_load(Window *window) {
   //Create GBitmap, then set to created BitmapLayer
-  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
-  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
-  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+//   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+//   s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+//   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+//   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
   
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
+  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
   
   //Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+//   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
 
   //Apply to TextLayer
-  text_layer_set_font(s_time_layer, s_time_font);
+//   text_layer_set_font(s_time_layer, fonts_get_system_font(
+//   FONT_KEY_BITHAM_24_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
@@ -60,10 +64,10 @@ static void main_window_load(Window *window) {
 
 static void main_window_unload(Window *window) {
   //Unload GFont
-  fonts_unload_custom_font(s_time_font);
+  //fonts_unload_custom_font(s_time_font);
   
   //Destroy GBitmap
-  gbitmap_destroy(s_background_bitmap);
+  // gbitmap_destroy(s_background_bitmap);
 
   //Destroy BitmapLayer
   bitmap_layer_destroy(s_background_layer);
@@ -94,7 +98,7 @@ static void init() {
   window_stack_push(s_main_window, true);
   
   // Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 }
 
 static void deinit() {
