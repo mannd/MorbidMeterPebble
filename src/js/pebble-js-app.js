a1351f1ab8e26602485dcc4579722c666affc324
David Mann <mannd@epstudiossoftware.com>
2015-10-27 22:55:34 -0600

    Added config files, Slate files via bower.  Started editing index.html.

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,29 @@
+Pebble.addEventListener('ready', function() {
+  console.log('PebbleKit JS ready!');
+});
+
+Pebble.addEventListener('showConfiguration', function() {
+//  var url = 'http://238.0.0.2:8080'p
+  var url = 'http://127.0.0.1:8080'
+
+  console.log('Showing configuration page: ' + url);
+
+  Pebble.openURL(url);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  var configData = JSON.parse(decodeURIComponent(e.response));
+
+  console.log('Configuration page returned: ' + JSON.stringify(configData));
+
+  if (configData.backgroundColor) {
+    Pebble.sendAppMessage({
+      backgroundColor: parseInt(configData.backgroundColor, 16),
+      twentyFourHourFormat: configData.twentyFourHourFormat
+    }, function() {
+      console.log('Send successful!');
+    }, function() {
+      console.log('Send failed!');
+    });
+  }
+});
