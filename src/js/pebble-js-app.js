b878fbbdddaa176211116bc3bb9309f68baaa5e4
David Mann <mannd@epstudiossoftware.com>
2015-11-29 23:38:19 -0700

    Change back to github.io page for config

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -4,10 +4,10 @@ Pebble.addEventListener('ready', function() {
 });
 
 Pebble.addEventListener('showConfiguration', function() {
-  var url = 'http://127.0.0.1:8080';
+//  var url = 'http://127.0.0.1:8080';
 //  var url = 'http://6adb98e7.ngrok.io';
 //  var url = 'http://epstudiossoftware.com/pebble/config/';
-//  var url = 'http://mannd.github.io/MorbidMeterPebble/';
+  var url = 'http://mannd.github.io/MorbidMeterPebble/';
   console.log('Showing configuration page: ' + url);
   Pebble.openURL(url);
 });
